408d15402f41c2d4b79405efd96c18e485193cd7
open-meadow <rohan.varughese@protonmail.com>
2023-01-23 19:18:05 +0000

    Remove commented out code

--- classes/questions.rb
+++ classes/questions.rb
@@ -1,20 +1,3 @@
-# @questions_p1 = [
-#   {
-#     id: 1,
-#     question: "What is 1 + 1?",
-#     answer: 2,
-#     correct_comment: "You're so smart!",
-#     wrong_comment: "It's okay, everybody starts somewhere!"
-#   },
-#   {
-#     id: 2,
-#     question: "What is p vs np?",
-#     answer: nil,
-#     correct_comment: nil,
-#     wrong_comment: "You're worthless!",
-#   }
-# ]
-
 class Questions
   def initialize
     @num1 = rand(21).to_i
